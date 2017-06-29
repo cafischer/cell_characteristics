626150dde458456c2b66625281700db343272146
caro <coralinefischer@gmail.com>
2017-06-29 16:20:51 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cell_characteristics',
+      version='0.1',
+      description='Automatic extraction of cell parameters',
+      url='https://github.com/cafischer/cell_characteristics',
+      author='Caroline Fischer',
+      author_email='coralinefischer@gmail.com',
+      license='',
+      packages=['cell_characteristics'],
+      zip_safe=False)
