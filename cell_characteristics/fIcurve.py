7f69b7a2d77ebbcf15686b4294018bbf42c4ff4b
cafischer <coralinefischer@gmail.com>
2017-07-12 16:16:18 +0200

    added alternative computation of fIcurve

--- cell_characteristics/fIcurve.py
+++ cell_characteristics/fIcurve.py
@@ -14,4 +14,19 @@ def compute_fIcurve(v_traces, i_traces, t_trace):
         AP_onsets = get_AP_onsets(v_traces[i], threshold=0)
         n_APs = len(AP_onsets)
         firing_rates[i] = n_APs / dur_step
-    return amps[amps >= 0], firing_rates[amps >= 0]
+    return amps, firing_rates
+
+
+def compute_fIcurve_last_ISI(v_traces, i_traces, t_trace):
+    start_step = np.nonzero(i_traces[0])[0][0]
+    amps = np.array([i_inj[start_step] for i_inj in i_traces])
+    dt = t_trace[1] - t_trace[0]
+
+    firing_rates = np.zeros(len(amps))
+    for i, amp in enumerate(amps):
+        AP_onsets = get_AP_onsets(v_traces[i], threshold=0)
+        if len(AP_onsets) >= 2:
+            firing_rates[i] = AP_onsets[-1] * dt - AP_onsets[-2] * dt
+        else:
+            firing_rates[i] = np.nan
+    return amps, firing_rates
